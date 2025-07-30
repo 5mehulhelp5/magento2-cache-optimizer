df49bafaefcb61c04b8716bcffb0d3ad9153ca2e
Manuel Rülke <github@lazerbahn.com>
2025-07-30 12:22:12 +0200

    Initial commit

---
+++ Plugin/ResponsePlugin.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Triplewood\CacheOptimizer\Plugin;
+
+use Magento\Customer\Model\Context as ContextModel;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Http\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Response\Http;
+use Magento\Store\Model\ScopeInterface;
+
+class ResponsePlugin
+{
+    public function __construct(
+        private readonly RequestInterface $request,
+        private readonly Context $httpContext,
+        private readonly ScopeConfigInterface $scopeConfig
+    ) {
+    }
+
+    public function aroundSendResponse(Http $subject, callable $proceed)
+    {
+        $isEnabled =  $this->scopeConfig->getValue(
+            'guest_cache_optimization/settings/active',
+            ScopeInterface::SCOPE_STORE
+        );
+
+        if (!$isEnabled) {
+            return $proceed();
+        }
+
+        $isLoggedIn = $this->httpContext->getValue(ContextModel::CONTEXT_AUTH);
+
+        if ($this->request->isGet()
+            && !$isLoggedIn
+            && !str_contains($this->request->getRequestUri(), '/checkout/')
+            && !str_contains($this->request->getRequestUri(), '/customer/')
+        ) {
+            $header = $this->scopeConfig->getValue(
+                'guest_cache_optimization/settings/cache_header',
+                ScopeInterface::SCOPE_STORE
+            ) ?? 'public, max-age=600, s-maxage=3600';
+
+            $subject->setHeader('Cache-Control', $header, true);
+            $subject->clearHeader('Pragma');
+            $subject->setHeader('Pragma', '', true);
+            $subject->clearHeader('Expires');
+            $subject->setHeader('Expires', '', true);
+        }
+
+        return $proceed();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# magento2-cache-optimizer
-Enable browser caching for users who are not logged in, for pages with non-sensitive data.
+Triplwood Cache Optimizer
+=========================
+
+Magento 2 sends the following cache-control header on HTML responses (not static assets):
+
+    Cache-Control: max-age=0, must-revalidate, no-cache, no-store
+
+This essentially instructs the browser not to cache any HTML pages at all. This means the browser will always
+re-request full page HTML, even on back/forward navigation.
+
+**Why Magento Does This**
+
+- Magento HTML often contains user-specific content (cart totals, customer name, wishlist count). Caching HTML in the browser risks showing stale data for logged-in users.
+- Magento relies on server-side caching (Varnish / built-in FPC), not browser cache, for performance.
+  -- Guest HTML pages are cached by FPC/Varnish Browsers should always revalidate, and FPC handles speed
+- Disabling browser caching helps prevent sensitive data (e.g., customer account pages, cart contents) from being stored in browser cache, especially in shared/public machines.
+
+**The consequences**
+
+Magento errs on the side of caution. Even for guests, where caching HTML might be fine, it globally disables
+caching to avoid complexity and edge cases. By disabling the browser cache, services like Cloudflare cannot apply their
+HTML caching techniques - making Magento run even slower.
+
+This module enables browser-caching for non-dynamic guest-pages:
+
+- CMS pages
+- product pages
+- category pages
+
+
